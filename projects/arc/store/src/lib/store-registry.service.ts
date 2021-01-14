521490b36e0bcc7711bee9b407b64c8d7e816d10
Moritz Brandstätter <arriggon@gmail.com>
2021-01-14 09:27:50 +0000

    change get-method signature

--- projects/arc/store/src/lib/store-registry.service.ts
+++ projects/arc/store/src/lib/store-registry.service.ts
@@ -30,7 +30,7 @@ export class StoreRegistry {
     this.registeredStores.delete(name);
   }
 
-  get(name: string): GenericStore<any> {
+  get(name: string): any {
     return this.registeredStores.get(name);
   }
 }
